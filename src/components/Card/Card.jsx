bbca783b35c1ee93caca40e56240f6b8b4a44a7b
Rizwan Ahmad <ahmadrizwan178@gmail.com>
2024-01-28 18:18:17 +0530

    admin-panel-2

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import './Card.css';
-import { LayoutGroup } from 'framer-motion';
+import { motion, LayoutGroup } from 'framer-motion';
 
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 import {UilTimes} from "@iconscout/react-unicons";
+import ReactApexChart from 'react-apexcharts';
 
 function Card(props) {
 
@@ -29,11 +30,12 @@ function Card(props) {
 function CompactCard({param, setExpnaded}){
     const Png = param.png;
     return(
-        <div className="CompactCard"
+        <motion.div className="CompactCard"
             style={{
                 background: param.color.backGround,
                 boxShadow: param.color.boxShadow
             }}
+            // layoutId='expandableCard'
             onClick={setExpnaded}
         >
             <div className="radialBar">
@@ -45,30 +47,80 @@ function CompactCard({param, setExpnaded}){
                 <span>${param.value}</span>
                 <span>Last 24 hours</span>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
 //ExpandedCard
 function ExpandedCard({param, setExpnaded}){
+    const data = {
+        options:{
+            chart:{
+                type: "area",
+                height: "auto",
+            },
+            dropShadow: {
+                enabled: false,
+                enabledOnSeries: undefined,
+                top: 0,
+                left: 0,
+                blur: 3,
+                color: "#000",
+                opacity: 0.35,
+            },
+            fill: {
+                colors: ["#fff"],
+                type: "gradient",
+              },
+              dataLabels: {
+                enabled: false,
+              },
+      
+            stroke: {
+                curve: "smooth",
+                colors: ["white"],
+            },
+            tooltip: {
+                x: {
+                    format: "dd/MM/yy HH:mm",
+                }
+            },
+            grid: {
+                show: true,
+            },
+            xaxis: {
+                type: "datetime",
+                categories: [
+                    "2018-09-19T00:00:00.000Z",
+                    "2018-09-19T01:00:00.000Z",
+                    "2018-09-19T02:00:00.000Z",
+                    "2018-09-19T03:00:00.000Z",
+                    "2018-09-19T04:00:00.000Z",
+                    "2018-09-19T05:00:00.000Z",
+                    "2018-09-19T06:00:00.000Z",
+                ]
+            }
+        }
+    }
     return (
-        <div className="ExpandedCard" 
+        <motion.div className="ExpandedCard" 
         style={{background: param.color.backGround,
-            boxShadow:param.color.boxShadow
+            boxShadow:param.color.boxShadow,
             }}
+            layoutId='expandableCard'
         >
-        <div>
+        <div style={{alignSelf: 'flex-end', cursor:'pointer', color:'white'}}>
             <UilTimes onClick={setExpnaded}/>    
         </div>    
         <span>
             {param.title}
         </span>
         <div className="chartContainer">
-            Chart
+            <ReactApexChart series={param.series} type='area' options={data.options}/>
         </div>
         <span>Last 24  hours</span>
 
-        </div>
+        </motion.div>
     )
 }
 
---
+++ src/components/RightSide/RightSide.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './RightSide.css';
+function RightSide() {
+  return (
+    <div>
+      RightSide
+    </div>
+  )
+}
+
+export default RightSide
